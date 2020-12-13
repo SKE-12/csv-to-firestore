df62604fb11e7ae32490daa94512f99a4b3f9559
Glauber Portella <glauberportella@gmail.com>
2020-12-13 20:57:13 -0300

    csv options

--- bin/lib/loadCSV.js
+++ bin/lib/loadCSV.js
@@ -1,7 +1,7 @@
 
 const USER_PATH = process.cwd()
 
-module.exports = csvPath, csvOptions => {
+module.exports = (csvPath, csvOptions) => {
     if (!csvPath) throw new ReferenceError('CSV PATH is NOT FOUND!!!')
     const spinner = require('ora')().start('Loading Data from CSV...')
     const data = require('csvtojson')(csvOptions).fromFile(`${USER_PATH}/${csvPath}`)
